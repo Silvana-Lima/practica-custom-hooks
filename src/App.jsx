fd754cb132e97057d5f857a09342f08f42a31abc
silvana02 <silvanalima.89@hotmail.com>
2023-04-30 01:10:53 +0200

    modifico ej dos

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ function App() {
       <Count />
       <CardNew />
       <FormUser />
+      {/* <h2>Ej 1 - Contador</h2>
+      <Count /> */}
+
+      <div>
+        <h2>Ej 2 - Últimas noticias</h2>
+        <CardNew />
+      </div>
     </>
   )
 }
--- src/components/CardNew.jsx
+++ src/components/CardNew.jsx
@@ -1,15 +1,25 @@
 import { useGetApi } from '../hooks/useGetApi'
 
 export const CardNew = () => {
-  const { data } = useGetApi('https://rickandmortyapi.com/api/character')
+  const { data } = useGetApi(
+    'https://newsapi.org/v2/top-headlines?country=ar&apiKey=087719249a154ecd89266e64e4b25079'
+  )
 
   return (
-    <div>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '15px' }}>
       {data &&
-        data.results.map((d) => (
-          <div key={d.id}>
-            <h3>{d.name} </h3>
-            <p>{d.status} </p>
+        data.articles.map((a) => (
+          <div
+            key={a.id}
+            style={{
+              width: '250px',
+              padding: '10px',
+              borderStyle: 'solid',
+              borderRadius: '10px',
+            }}
+          >
+            <h3>{a.title} </h3>
+            <p>{a.author} </p>
           </div>
         ))}
     </div>
